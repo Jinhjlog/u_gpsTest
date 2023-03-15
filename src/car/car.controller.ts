9f0733b7abdab19801b15d18d8a59d3a68efeba6
Seonzoo <boy672820@gmail.com>
2023-03-15 16:03:46 +0900

    rebase main

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -1,12 +1,37 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CarService } from './car.service';
 
-@Controller('')
+@Controller('car')
 export class CarController {
   constructor(private readonly carService: CarService) {}
 
-  @Get('getCarLocation')
+  @Post('/api/getCarLocation')
   getCarLocation(@Body() data) {
     return this.carService.getCarLocation(data);
+  }
+
+  @Post('/api/getCarSttus')
+  getCarSttus() {
+    return {
+      rsltCd: '000',
+      infoList: [
+        {
+          carNum: '01가1001',
+          carStCd: '1',
+        },
+        {
+          carNum: '01가1002',
+          carStCd: '1',
+        },
+        {
+          carNum: '01-MB-0001',
+          carStCd: '1',
+        },
+        {
+          carNum: '01가1003',
+          carStCd: '0',
+        },
+      ],
+    };
   }
 }
